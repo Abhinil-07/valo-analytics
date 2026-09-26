2a0c99404b50b2e75a84c68781eb052af3b988b7
Abhinil Nath <abhinilnath10@gmail.com>
2026-09-26 11:58:34 +0530

    feat(silver): add Summit and Corrode maps with official Riot CDN assets to dim_map

--- release_1/silver/03_dim_map.py
+++ release_1/silver/03_dim_map.py
@@ -56,12 +56,14 @@ map_seed_data = [
     ("Ascent", 2, ['A', 'B'], "https://media.valorant-api.com/maps/7eaecc1b-4337-bbf6-6ab9-04b8f06b3319/splash.png", "https://media.valorant-api.com/maps/7eaecc1b-4337-bbf6-6ab9-04b8f06b3319/displayicon.png", True),
     ("Bind", 2, ['A', 'B'], "https://media.valorant-api.com/maps/2c9d57ec-4431-9c5e-2939-8f9ef6dd5cba/splash.png", "https://media.valorant-api.com/maps/2c9d57ec-4431-9c5e-2939-8f9ef6dd5cba/displayicon.png", True),
     ("Breeze", 2, ['A', 'B'], "https://media.valorant-api.com/maps/2fb9a4fd-47b8-4e7d-a969-74b4046ebd53/splash.png", "https://media.valorant-api.com/maps/2fb9a4fd-47b8-4e7d-a969-74b4046ebd53/displayicon.png", True),
+    ("Corrode", 2, ['A', 'B'], "https://media.valorant-api.com/maps/1c18ab1f-420d-0d8b-71d0-77ad3c439115/splash.png", "https://media.valorant-api.com/maps/1c18ab1f-420d-0d8b-71d0-77ad3c439115/displayicon.png", True),
     ("Fracture", 2, ['A', 'B'], "https://media.valorant-api.com/maps/b529448b-4d60-346e-e89e-00a4c527a405/splash.png", "https://media.valorant-api.com/maps/b529448b-4d60-346e-e89e-00a4c527a405/displayicon.png", True),
     ("Haven", 3, ['A', 'B', 'C'], "https://media.valorant-api.com/maps/2bee0dc9-4ffe-519b-1cbd-7fbe763a6047/splash.png", "https://media.valorant-api.com/maps/2bee0dc9-4ffe-519b-1cbd-7fbe763a6047/displayicon.png", True),
     ("Icebox", 2, ['A', 'B'], "https://media.valorant-api.com/maps/e2ad5c54-4114-a870-9641-8ea21279579a/splash.png", "https://media.valorant-api.com/maps/e2ad5c54-4114-a870-9641-8ea21279579a/displayicon.png", True),
     ("Lotus", 3, ['A', 'B', 'C'], "https://media.valorant-api.com/maps/2fe4ed3a-450a-948b-6d6b-e89a78e680a9/splash.png", "https://media.valorant-api.com/maps/2fe4ed3a-450a-948b-6d6b-e89a78e680a9/displayicon.png", True),
     ("Pearl", 2, ['A', 'B'], "https://media.valorant-api.com/maps/fd267378-4d1d-484f-ff52-77821ed10dc2/splash.png", "https://media.valorant-api.com/maps/fd267378-4d1d-484f-ff52-77821ed10dc2/displayicon.png", True),
     ("Split", 2, ['A', 'B'], "https://media.valorant-api.com/maps/d960549e-485c-e861-8d71-aa9d1aed12a2/splash.png", "https://media.valorant-api.com/maps/d960549e-485c-e861-8d71-aa9d1aed12a2/displayicon.png", True),
+    ("Summit", 2, ['A', 'B'], "https://media.valorant-api.com/maps/756da597-416b-c0f2-f47b-afbdf28670bc/splash.png", "https://media.valorant-api.com/maps/756da597-416b-c0f2-f47b-afbdf28670bc/displayicon.png", True),
     ("Sunset", 2, ['A', 'B'], "https://media.valorant-api.com/maps/92584fbe-486a-b1b2-9faa-39b0f486b498/splash.png", "https://media.valorant-api.com/maps/92584fbe-486a-b1b2-9faa-39b0f486b498/displayicon.png", True),
 ]
 
